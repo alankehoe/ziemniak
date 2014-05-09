2cc04ece8ba904c238c391df98c1aeb315ad5b29
Alan Kehoe <alankehoe111@gmail.com>
2014-05-09 11:09:53 +0100

    Use bower for dependencies

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,9 +23,9 @@ Ziemniak::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.compress = true
-  config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :scss
+  # config.assets.compress = true
+  # config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :scss
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
