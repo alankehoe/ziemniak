273cc6de74929235bec8f5d2c36806f522f06f20
Alan Kehoe <alankehoe111@gmail.com>
2014-05-12 21:50:16 +0100

    Add trianglfy for some cool backgrounds

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,6 +9,7 @@
 //= require ../../../vendor/assets/bower_components/angular-touch/angular-touch.js
 //= require ../../../vendor/assets/bower_components/angular-bootstrap/ui-bootstrap.js
 //= require ../../../vendor/assets/bower_components/d3/d3.js
+//= require ../../../vendor/assets/bower_components/trianglify/trianglify.js
 
 // Other libraries
 //= require ../../../vendor/assets/javascripts/pie-chart/pie-chart.js
