1a0d499eefd8bfa3949c977d447059b4a2a91327
Alan Kehoe <alankehoe111@gmail.com>
2014-06-03 22:44:07 +0100

    Fix dependency

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,11 @@
 // Bower libraries
 //= require ../../../vendor/assets/bower_components/angular/angular.js
 //= require ../../../vendor/assets/bower_components/angular-animate/angular-animate.js
-//= require ../../../vendor/assets/bower_components/angular-cookies/angular-cookies.js
-//= require ../../../vendor/assets/bower_components/angular-loader/angular-loader.js
 //= require ../../../vendor/assets/bower_components/angular-resource/angular-resource.js
 //= require ../../../vendor/assets/bower_components/angular-route/angular-route.js
-//= require ../../../vendor/assets/bower_components/angular-sanitize/angular-sanitize.js
-//= require ../../../vendor/assets/bower_components/angular-touch/angular-touch.js
 //= require ../../../vendor/assets/bower_components/angular-bootstrap/ui-bootstrap.js
+//= require ../../../vendor/assets/bower_components/angular-bootstrap/ui-bootstrap-tpls.js
 //= require ../../../vendor/assets/bower_components/d3/d3.js
-//= require ../../../vendor/assets/bower_components/trianglify/trianglify.js
 
 // Other libraries
 //= require ../../../vendor/assets/javascripts/pie-chart/pie-chart.js
